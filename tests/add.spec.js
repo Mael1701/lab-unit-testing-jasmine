33857b493b40bf58a07ddfa781906508490b4180
Mael Micout <gh api micoutmael3@gmail.com>
2024-07-19 17:03:40 +0200

    Solved lab

--- tests/add.spec.js
+++ tests/add.spec.js
@@ -3,17 +3,23 @@ describe("Iteration 1 | Getting Started", () => {
       it("should be defined", () => {
         expect(add).toBeDefined();
       });
-  
+
       it("should take two arguments", () => {
         expect(add.length).toBe(2);
       });
-  
+
       it("should return the sum of the two numbers", () => {
         expect(add(1, 2)).toEqual(3);
         expect(add(3, 4)).toEqual(7);
         expect(add(100, 47)).toEqual(147);
       });
-  
+
+      it("should return undefined if any of the two arguments is not a number", () => {
+        expect(add("1", 2)).toEqual(undefined);
+        expect(add(3, "4")).toEqual(undefined);
+        expect(add("100", "47")).toEqual(undefined);
+      });
+
       it("should return undefined if any of the arguments is not provided", () => {
         expect(add(1)).toEqual(undefined);
         expect(add()).toEqual(undefined);
@@ -21,4 +27,3 @@ describe("Iteration 1 | Getting Started", () => {
       });
     });
   });
-  
